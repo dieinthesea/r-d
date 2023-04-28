095c549e3bf965fe650294014a79be746bcd717d
Universe51129 <91587729+Universe51129@users.noreply.github.com>
2023-04-28 16:51:31 +0800

    Update QTSSPosixFileSysModule.cpp

--- DarwinStreamingSrvr5.5.5-Source/APIModules/QTSSPOSIXFileSysModule/QTSSPosixFileSysModule.cpp
+++ DarwinStreamingSrvr5.5.5-Source/APIModules/QTSSPOSIXFileSysModule/QTSSPosixFileSysModule.cpp
@@ -1,38 +1,3 @@
-/*
- *
- * @APPLE_LICENSE_HEADER_START@
- * 
- * Copyright (c) 1999-2003 Apple Computer, Inc.  All Rights Reserved.
- * 
- * This file contains Original Code and/or Modifications of Original Code
- * as defined in and that are subject to the Apple Public Source License
- * Version 2.0 (the 'License'). You may not use this file except in
- * compliance with the License. Please obtain a copy of the License at
- * http://www.opensource.apple.com/apsl/ and read it before using this
- * file.
- * 
- * The Original Code and all software distributed under the License are
- * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
- * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
- * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
- * Please see the License for the specific language governing rights and
- * limitations under the License.
- * 
- * @APPLE_LICENSE_HEADER_END@
- *
- */
-/*
-    File:       QTSSPosixFileSysModule.cpp
-
-    Contains:   Implements web stats module
-
-    Written By: Denis Serenyi
-
-    
-
-*/
-
 #include "QTSSPosixFileSysModule.h"
 #include "QTSSModuleUtils.h"
 
@@ -147,7 +112,9 @@ QTSS_Error  OpenFile(QTSS_OpenFile_Params* inParams)
     }
 
     //
-    // If caller wants async I/O, at this point we should set up the EventContext
+    //Set up the EventContext to ensure async I/O
+    //Get sockets by 'GetEventThread()'
+    
     if (inParams->inFlags & qtssOpenFileAsync)
     {
         EventContext* theEventContext = NEW EventContext(EventContext::kInvalidFileDesc, Socket::GetEventThread());
@@ -221,7 +188,7 @@ QTSS_Error  CloseFile(QTSS_CloseFile_Params* inParams)
     delete *theFile;
     return QTSS_NoErr;
 }
-
+//get data from main thread
 QTSS_Error  RequestEventFile(QTSS_RequestEventFile_Params* inParams)
 {
     QTSS_ModuleState* theState = (QTSS_ModuleState*)OSThread::GetMainThreadData();
