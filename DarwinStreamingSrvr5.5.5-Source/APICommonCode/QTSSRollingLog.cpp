425a5cc86de060afe27aafa9b4f431aaefa1d1f5
Universe51129 <91587729+Universe51129@users.noreply.github.com>
2023-04-28 15:29:05 +0800

    Update QTSSRollingLog.cpp

--- DarwinStreamingSrvr5.5.5-Source/APICommonCode/QTSSRollingLog.cpp
+++ DarwinStreamingSrvr5.5.5-Source/APICommonCode/QTSSRollingLog.cpp
@@ -1,35 +1,3 @@
-/*
- *
- * @APPLE_LICENSE_HEADER_START@
- * 
- * Copyright (c) 1999-2003 Apple Computer, Inc.  All Rights Reserved.
- * 
- * This file contains Original Code and/or Modifications of Original Code
- * as defined in and that are subject to the Apple Public Source License
- * Version 2.0 (the 'License'). You may not use this file except in
- * compliance with the License. Please obtain a copy of the License at
- * http://www.opensource.apple.com/apsl/ and read it before using this
- * file.
- * 
- * The Original Code and all software distributed under the License are
- * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
- * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
- * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
- * Please see the License for the specific language governing rights and
- * limitations under the License.
- * 
- * @APPLE_LICENSE_HEADER_END@
- *
- */
-/*
-    File:       QTSSRollingLog.cpp
-
-    Contains:   Implements object defined in .h file
-
-
-*/
-
 #include <time.h>
 #include <stdlib.h>
 #include "SafeStdLib.h"
@@ -339,8 +307,7 @@ Bool16 QTSSRollingLog::RenameLogFile(const char* inFileName)
         //add the log number & suffix
         qtss_sprintf(theNewNameBuffer + theBaseNameLength, "%03ld.log", x);
 
-        //assume that when ::stat returns an error, it is becase
-        //the file doesnt exist. Once that happens, we have a unique name
+        //When ::stat returns an error that the file doesn't exist. Once that happens, we have a unique name
         // csl - shouldn't you watch for a ENOENT result?
         struct stat theIdontCare;
         theErr = ::stat(theNewNameBuffer, &theIdontCare);
