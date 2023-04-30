8ab0f20a9f8d57c75c1a8b25cf7d59830a70b81a
Universe51129 <91587729+Universe51129@users.noreply.github.com>
2023-04-30 21:23:11 +0800

    Update MyAssert.h

--- DarwinStreamingSrvr5.5.5-Source/CommonUtilitiesLib/MyAssert.h
+++ DarwinStreamingSrvr5.5.5-Source/CommonUtilitiesLib/MyAssert.h
@@ -1,28 +1,3 @@
-/*
- *
- * @APPLE_LICENSE_HEADER_START@
- * 
- * Copyright (c) 1999-2003 Apple Computer, Inc.  All Rights Reserved.
- * 
- * This file contains Original Code and/or Modifications of Original Code
- * as defined in and that are subject to the Apple Public Source License
- * Version 2.0 (the 'License'). You may not use this file except in
- * compliance with the License. Please obtain a copy of the License at
- * http://www.opensource.apple.com/apsl/ and read it before using this
- * file.
- * 
- * The Original Code and all software distributed under the License are
- * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
- * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
- * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
- * Please see the License for the specific language governing rights and
- * limitations under the License.
- * 
- * @APPLE_LICENSE_HEADER_END@
- *
- */
-
 #ifndef _MYASSERT_H_
 #define _MYASSERT_H_
 
@@ -33,11 +8,9 @@
 class AssertLogger
 {
     public:
-        // An interface so the MyAssert function can write a message
         virtual void LogAssert(char* inMessage) = 0;
 };
 
-// If a logger is provided, asserts will be logged. Otherwise, asserts will cause a bus error
 void SetAssertLogger(AssertLogger* theLogger);
 #endif
 
