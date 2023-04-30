9a9fc6dbef39019c1c49530334a0db6249bf5803
Universe51129 <91587729+Universe51129@users.noreply.github.com>
2023-04-30 21:59:12 +0800

    Update OSArrayObjectDeleter.h

--- DarwinStreamingSrvr5.5.5-Source/CommonUtilitiesLib/OSArrayObjectDeleter.h
+++ DarwinStreamingSrvr5.5.5-Source/CommonUtilitiesLib/OSArrayObjectDeleter.h
@@ -1,36 +1,3 @@
-/*
- *
- * @APPLE_LICENSE_HEADER_START@
- * 
- * Copyright (c) 1999-2003 Apple Computer, Inc.  All Rights Reserved.
- * 
- * This file contains Original Code and/or Modifications of Original Code
- * as defined in and that are subject to the Apple Public Source License
- * Version 2.0 (the 'License'). You may not use this file except in
- * compliance with the License. Please obtain a copy of the License at
- * http://www.opensource.apple.com/apsl/ and read it before using this
- * file.
- * 
- * The Original Code and all software distributed under the License are
- * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
- * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
- * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
- * Please see the License for the specific language governing rights and
- * limitations under the License.
- * 
- * @APPLE_LICENSE_HEADER_END@
- *
- */
-/*
-    File:       OSArrayObjectDeleter.h
-
-    Contains:   Auto object for deleting arrays.
-
-
-
-*/
-
 #ifndef __OS_ARRAY_OBJECT_DELETER_H__
 #define __OS_ARRAY_OBJECT_DELETER_H__
 
@@ -83,4 +50,4 @@ class OSPtrDeleter
 typedef OSArrayObjectDeleter<char*> OSCharPointerArrayDeleter;
 typedef OSArrayObjectDeleter<char> OSCharArrayDeleter;
 
-#endif //__OS_OBJECT_ARRAY_DELETER_H__
+#endif 
