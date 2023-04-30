6ac661bcdf2856863236a18dc5585df3db2f848d
Sylvia-qiu <91591335+Sylvia-qiu@users.noreply.github.com>
2023-04-30 15:57:45 +0800

    Update and rename DarwinStreamingSrvr5.5.5-Source/APICommonCode/QTSSModuleUtils.h to DarwinStreamingSrvr5.5.5-Source/API目录代码/QTSSModuleUtils.h
    更改注释

--- DarwinStreamingSrvr5.5.5-Source/API目录代码/QTSSModuleUtils.h
+++ DarwinStreamingSrvr5.5.5-Source/API目录代码/QTSSModuleUtils.h
@@ -1,27 +1,9 @@
 /*
- *
- * @APPLE_LICENSE_HEADER_START@
- * 
- * Copyright (c) 1999-2003 Apple Computer, Inc.  All Rights Reserved.
- * 
- * This file contains Original Code and/or Modifications of Original Code
- * as defined in and that are subject to the Apple Public Source License
- * Version 2.0 (the 'License'). You may not use this file except in
- * compliance with the License. Please obtain a copy of the License at
- * http://www.opensource.apple.com/apsl/ and read it before using this
- * file.
- * 
- * The Original Code and all software distributed under the License are
- * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
- * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
- * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
- * Please see the License for the specific language governing rights and
- * limitations under the License.
- * 
- * @APPLE_LICENSE_HEADER_END@
- *
- */
+  The methods of this class include initialising a static array of characters, 
+  getting a copy of the accessed file, checking if access is allowed and authorising the request. 
+  In addition, this class defines an internal class called IPComponentStr, 
+  which is used to handle components of IP addresses.
+*/
 /*
     File:       QTSSModuleUtils.h
 
@@ -139,7 +121,6 @@ class QTSSModuleUtils
                 // Allocates memory - remember to delete it!
                 static char* CoalesceVectors(iovec* inVec, UInt32 inNumVectors, UInt32 inTotalLength);
                                                                                                                                                     
-        //
         // SEARCH FOR A SPECIFIC MODULE OBJECT                          
         static QTSS_ModulePrefsObject GetModuleObjectByName(const StrPtrLen& inModuleName);
         
@@ -193,10 +174,8 @@ class QTSSModuleUtils
         // or qtssIllegalAttrID if it isn't found.
         static QTSS_AttributeID GetAttrID(QTSS_Object inObject, char* inAttributeName);
         
-        //
-        //
-        //
-        /// Get the type of request. Returns qtssActionFlagsNoFlags on failure.
+       
+        //  Get the type of request. Returns qtssActionFlagsNoFlags on failure.
         //  Result is a bitmap of flags
         //
         static QTSS_ActionFlags GetRequestActions(QTSS_RTSPRequestObject theRTSPRequest);
