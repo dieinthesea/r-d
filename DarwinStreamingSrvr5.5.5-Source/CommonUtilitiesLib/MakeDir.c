f99acb68414275e0725712a7a76f67b844de0aa5
Universe51129 <91587729+Universe51129@users.noreply.github.com>
2023-04-30 21:20:40 +0800

    Update MakeDir.c

--- DarwinStreamingSrvr5.5.5-Source/CommonUtilitiesLib/MakeDir.c
+++ DarwinStreamingSrvr5.5.5-Source/CommonUtilitiesLib/MakeDir.c
@@ -1,30 +1,3 @@
-
-/*
- *
- * @APPLE_LICENSE_HEADER_START@
- * 
- * Copyright (c) 1999-2003 Apple Computer, Inc.  All Rights Reserved.
- * 
- * This file contains Original Code and/or Modifications of Original Code
- * as defined in and that are subject to the Apple Public Source License
- * Version 2.0 (the 'License'). You may not use this file except in
- * compliance with the License. Please obtain a copy of the License at
- * http://www.opensource.apple.com/apsl/ and read it before using this
- * file.
- * 
- * The Original Code and all software distributed under the License are
- * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
- * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
- * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
- * Please see the License for the specific language governing rights and
- * limitations under the License.
- * 
- * @APPLE_LICENSE_HEADER_END@
- *
- */
-
-
 #include "MakeDir.h"
 
 #include "PathDelimiter.h"
@@ -52,15 +25,15 @@ int MakeDir(const char* inPath, int mode)
     struct stat theStatBuffer;
     if (stat(inPath, &theStatBuffer) == -1)
     {
-        //this directory doesn't exist, so let's try to create it
+        //create a directory
         if (mkdir(inPath, mode) == -1)
-            return  -1; //€- (QTSS_ErrorCode)OSThread::GetErrno();
+            return  -1; //â‚¬- (QTSS_ErrorCode)OSThread::GetErrno();
     }
     else if (!S_ISDIR(theStatBuffer.st_mode))
-        return  -1; //€- QTSS_FileExists;//there is a file at this point in the path!
+        return  -1; //â‚¬- QTSS_FileExists;
 
     //directory exists
-    return  0; //€- QTSS_NoErr;
+    return  0; //â‚¬- QTSS_NoErr;
 }
 
 int RecursiveMakeDir(const char* inPath, int mode)
@@ -75,11 +48,8 @@ int RecursiveMakeDir(const char* inPath, int mode)
     if ( strlen( inPath ) > 255 )
         return -1;
     
-    //iterate through the path, replacing kPathDelimiterChar with '\0' as we go
-    
     strcpy( pathCopy, inPath );
     
-    //skip over the first / in the path.
     if (*thePathTraverser == kPathDelimiterChar )
         thePathTraverser++;
         
@@ -87,11 +57,8 @@ int RecursiveMakeDir(const char* inPath, int mode)
     {
         if (*thePathTraverser == kPathDelimiterChar)
         {
-            //we've found a filename divider. Now that we have a complete
-            //filename, see if this partial path exists.
+            //find a filename divider and complete filename, see if this partial path exists.
             
-            //make the partial path into a C string
-            // mkdir does not like a trailing '/'
             oldChar = *thePathTraverser;
             *thePathTraverser = '\0';
             theErr = MakeDir(pathCopy, mode);
