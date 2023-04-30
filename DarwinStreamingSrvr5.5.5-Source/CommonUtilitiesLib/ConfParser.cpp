5cdd0b39578b2a41866b720bae8828eab8e70d3f
Universe51129 <91587729+Universe51129@users.noreply.github.com>
2023-04-30 15:40:20 +0800

    Update ConfParser.cpp

--- DarwinStreamingSrvr5.5.5-Source/CommonUtilitiesLib/ConfParser.cpp
+++ DarwinStreamingSrvr5.5.5-Source/CommonUtilitiesLib/ConfParser.cpp
@@ -1,29 +1,3 @@
-/*
- *
- * @APPLE_LICENSE_HEADER_START@
- * 
- * Copyright (c) 1999-2003 Apple Computer, Inc.  All Rights Reserved.
- * 
- * This file contains Original Code and/or Modifications of Original Code
- * as defined in and that are subject to the Apple Public Source License
- * Version 2.0 (the 'License'). You may not use this file except in
- * compliance with the License. Please obtain a copy of the License at
- * http://www.opensource.apple.com/apsl/ and read it before using this
- * file.
- * 
- * The Original Code and all software distributed under the License are
- * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
- * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
- * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
- * Please see the License for the specific language governing rights and
- * limitations under the License.
- * 
- * @APPLE_LICENSE_HEADER_END@
- *
- */
-
-
 #include "ConfParser.h"
 #include "OSMemory.h"
 
@@ -146,18 +120,13 @@ int ParseConfigFile(
                                 
                 if ( *next == '#' )
                 {
-                    // it's a comment
-                    // prabably do nothing in release version?
 
-                    //  qtss_printf( "comment: %s" , &lineBuff[1] );
-                    
                     error = 0;
                     
                 }
                 else
                 {   char* param;
-                
-                    // grab the param name, quoted or not
+               
                     if ( *next == '"' )
                         next = GetQuotedWord( wordBuff, next, wordBuffSize );
                     else
@@ -181,7 +150,6 @@ int ParseConfigFile(
                         
                         while ( maxValues < kConfParserMaxParamValues && *next )
                         {
-                            // ace
                             next = TrimLeft( next );
                             
                             if (*next)
