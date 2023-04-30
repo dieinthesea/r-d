44f48f69250868459d605282518860151dd977b0
Universe51129 <91587729+Universe51129@users.noreply.github.com>
2023-04-30 16:34:40 +0800

    Update EventContext.h

--- DarwinStreamingSrvr5.5.5-Source/CommonUtilitiesLib/EventContext.h
+++ DarwinStreamingSrvr5.5.5-Source/CommonUtilitiesLib/EventContext.h
@@ -1,39 +1,3 @@
-/*
- *
- * @APPLE_LICENSE_HEADER_START@
- * 
- * Copyright (c) 1999-2003 Apple Computer, Inc.  All Rights Reserved.
- * 
- * This file contains Original Code and/or Modifications of Original Code
- * as defined in and that are subject to the Apple Public Source License
- * Version 2.0 (the 'License'). You may not use this file except in
- * compliance with the License. Please obtain a copy of the License at
- * http://www.opensource.apple.com/apsl/ and read it before using this
- * file.
- * 
- * The Original Code and all software distributed under the License are
- * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
- * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
- * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
- * Please see the License for the specific language governing rights and
- * limitations under the License.
- * 
- * @APPLE_LICENSE_HEADER_END@
- *
- */
-/*
-    File:       EventContext.h
-
-    Contains:   An event context provides the intelligence to take an event
-                generated from a UNIX file descriptor (usually EV_RE or EV_WR)
-                and signal a Task. 
-                    
-
-    
-    
-*/
-
 #ifndef __EVENT_CONTEXT_H__
 #define __EVENT_CONTEXT_H__
 
@@ -51,23 +15,15 @@ class EventContext
 {
     public:
     
-        //
-        // Constructor. Pass in the EventThread you would like to receive
-        // events for this context, and the fd that this context applies to
+        //Pass in the EventThread received,events for this context, and the fd that this context applies to
         EventContext(int inFileDesc, EventThread* inThread);
         virtual ~EventContext() { if (fAutoCleanup) this->Cleanup(); }
-        
-        //
-        // InitNonBlocking
-        //
-        // Sets inFileDesc to be non-blocking. Once this is called, the
-        // EventContext object "owns" the file descriptor, and will close it
-        // when Cleanup is called. This is necessary because of some weird
-        // select() behavior. DON'T CALL CLOSE ON THE FD ONCE THIS IS CALLED!!!!
+    
+        // Sets inFileDesc to be non-blocking. Once this is called, the EventContext object "owns" the file descriptor, and will close it when Cleanup is called. This is necessary because of the select() behavior.
         void            InitNonBlocking(int inFileDesc);
 
         //
-        // Cleanup. Will be called by the destructor, but can be called earlier
+        // Cleanup.
         void            Cleanup();
 
         //
@@ -75,8 +31,6 @@ class EventContext
         void            RequestEvent(int theMask = EV_RE);
 
         
-        //
-        // Provide the task you would like to be notified
         void            SetTask(Task* inTask)
         {  
             fTask = inTask; 
@@ -89,14 +43,12 @@ class EventContext
             }
         }
         
-        // when the HTTP Proxy tunnels takes over a TCPSocket, we need to maintain this context too
         void            SnarfEventContext( EventContext &fromContext );
         
         // Don't cleanup this socket automatically
         void            DontAutoCleanup() { fAutoCleanup = false; }
         
-        // Direct access to the FD is not recommended, but is needed for modules
-        // that want to use the Socket classes and need to request events on the fd.
+        // Direct access to the FD is not recommended, but is needed for modules that want to use the Socket classes and need to request events on the fd.
         int             GetSocketFD()       { return fFileDesc; }
         
         enum
@@ -109,17 +61,14 @@ class EventContext
         //
         // ProcessEvent
         //
-        // When an event occurs on this file descriptor, this function
-        // will get called. Default behavior is to Signal the associated
-        // task, but that behavior may be altered / overridden.
-        //
-        // Currently, we always generate a Task::kReadEvent
+        // When an event occurs on this file descriptor, this function will get called.
+        // Generate a Task::kReadEvent
 		virtual void ProcessEvent(int /*eventBits*/) 
         {   
             if (EVENTCONTEXT_DEBUG)
             {
                 if (fTask== NULL)  
-                    qtss_printf("EventContext::ProcessEvent context=%lu task=NULL\n",(UInt32) this); 
+                   qtss_printf("EventContext::ProcessEvent context=%lu task=NULL\n",(UInt32) this); 
                 else 
                     qtss_printf("EventContext::ProcessEvent context=%lu task=%lu TaskName=%s\n",(UInt32)this,(UInt32) fTask, fTask->fTaskName); 
             }
