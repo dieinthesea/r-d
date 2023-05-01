2ca38a233e3a5a106e41ac4b6a255b3f9bee50b8
Universe51129 <91587729+Universe51129@users.noreply.github.com>
2023-05-01 16:23:45 +0800

    Update OSMutexRW.h

--- DarwinStreamingSrvr5.5.5-Source/CommonUtilitiesLib/OSMutexRW.h
+++ DarwinStreamingSrvr5.5.5-Source/CommonUtilitiesLib/OSMutexRW.h
@@ -1,36 +1,3 @@
-/*
- *
- * @APPLE_LICENSE_HEADER_START@
- * 
- * Copyright (c) 1999-2003 Apple Computer, Inc.  All Rights Reserved.
- * 
- * This file contains Original Code and/or Modifications of Original Code
- * as defined in and that are subject to the Apple Public Source License
- * Version 2.0 (the 'License'). You may not use this file except in
- * compliance with the License. Please obtain a copy of the License at
- * http://www.opensource.apple.com/apsl/ and read it before using this
- * file.
- * 
- * The Original Code and all software distributed under the License are
- * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
- * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
- * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
- * Please see the License for the specific language governing rights and
- * limitations under the License.
- * 
- * @APPLE_LICENSE_HEADER_END@
- *
- */
-/*
-    File:       OSMutexRW.h
-
-    Contains:   
-
-    
-
-*/
-
 #ifndef _OSMUTEXRW_H_
 #define _OSMUTEXRW_H_
 
@@ -54,7 +21,6 @@ class OSMutexRW
         void LockWrite();
         void Unlock();
         
-        // Returns 0 on success, EBUSY on failure
         int TryLockWrite();
         int TryLockRead();
     
@@ -62,7 +28,7 @@ class OSMutexRW
         enum {eMaxWait = 0x0FFFFFFF, eMultiThreadCondition = true, };
         enum {eActiveWriterState = -1, eNoWriterState = 0 };
 
-        OSMutex             fInternalLock;   // the internal lock         
+        OSMutex             fInternalLock;     
         OSCond              fReadersCond;    // the waiting readers             
         OSCond              fWritersCond;    // the waiting writers             
         int                 fState;          // -1:writer,0:free,>0:readers 
@@ -132,4 +98,4 @@ class   OSMutexWriteLocker: public OSMutexReadWriteLocker
 
 
 
-#endif //_OSMUTEX_H_
+#endif
