8724c0cce0971350fc17e5fb362785230caa70d8
Universe51129 <91587729+Universe51129@users.noreply.github.com>
2023-05-01 21:26:59 +0800

    Update OSThread.h

--- DarwinStreamingSrvr5.5.5-Source/CommonUtilitiesLib/OSThread.h
+++ DarwinStreamingSrvr5.5.5-Source/CommonUtilitiesLib/OSThread.h
@@ -1,37 +1,3 @@
-/*
- *
- * @APPLE_LICENSE_HEADER_START@
- * 
- * Copyright (c) 1999-2003 Apple Computer, Inc.  All Rights Reserved.
- * 
- * This file contains Original Code and/or Modifications of Original Code
- * as defined in and that are subject to the Apple Public Source License
- * Version 2.0 (the 'License'). You may not use this file except in
- * compliance with the License. Please obtain a copy of the License at
- * http://www.opensource.apple.com/apsl/ and read it before using this
- * file.
- * 
- * The Original Code and all software distributed under the License are
- * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
- * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
- * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
- * Please see the License for the specific language governing rights and
- * limitations under the License.
- * 
- * @APPLE_LICENSE_HEADER_END@
- *
- */
-/*
-    File:       OSThread.h
-
-    Contains:   A thread abstraction
-
-    
-
-*/
-
-// OSThread.h
 #ifndef __OSTHREAD__
 #define __OSTHREAD__
 
@@ -56,14 +22,12 @@ class OSThread
 {
 
 public:
-                //
                 // Call before calling any other OSThread function
                 static void     Initialize();
                 
                                 OSThread();
     virtual                     ~OSThread();
     
-    //
     // Derived classes must implement their own entry function
     virtual     void            Entry() = 0;
                 void            Start();
@@ -79,7 +43,6 @@ public:
                 void*           GetThreadData()         { return fThreadData; }
                 void            SetThreadData(void* inThreadData) { fThreadData = inThreadData; }
                 
-                // As a convienence to higher levels, each thread has its own date buffer
                 DateBuffer*     GetDateBuffer()         { return &fDateBuffer; }
                 
                 static void*    GetMainThreadData()     { return sMainThreadData; }
