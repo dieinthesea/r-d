30245bcbd819df5386e2ad623f0b10a10486c2bc
Universe51129 <91587729+Universe51129@users.noreply.github.com>
2023-05-01 15:24:15 +0800

    Update OSHashTable.h

--- DarwinStreamingSrvr5.5.5-Source/CommonUtilitiesLib/OSHashTable.h
+++ DarwinStreamingSrvr5.5.5-Source/CommonUtilitiesLib/OSHashTable.h
@@ -1,37 +1,3 @@
-/*
- *
- * @APPLE_LICENSE_HEADER_START@
- * 
- * Copyright (c) 1999-2003 Apple Computer, Inc.  All Rights Reserved.
- * 
- * This file contains Original Code and/or Modifications of Original Code
- * as defined in and that are subject to the Apple Public Source License
- * Version 2.0 (the 'License'). You may not use this file except in
- * compliance with the License. Please obtain a copy of the License at
- * http://www.opensource.apple.com/apsl/ and read it before using this
- * file.
- * 
- * The Original Code and all software distributed under the License are
- * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
- * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
- * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
- * Please see the License for the specific language governing rights and
- * limitations under the License.
- * 
- * @APPLE_LICENSE_HEADER_END@
- *
- */
-/*
-    File:       OSHashTable.h
-
-    Contains:  Defines a template class for hash tables.
-                        
-
-    
-
-*/
-
 #ifndef _OSHASHTABLE_H_
 #define _OSHASHTABLE_H_
 
@@ -47,9 +13,7 @@ public:
         Assert( fHashTable );
         memset( fHashTable, 0, sizeof(T*) * size );
         fSize = size;
-        // Determine whether the hash size is a power of 2
-        // if not set the mask to zero, otherwise we can
-        // use the mask which is faster for ComputeIndex
+       
         fMask = fSize - 1;
         if ((fMask & fSize) != 0)
             fMask = 0;
@@ -159,4 +123,4 @@ private:
     T* fCurrent;
     UInt32 fIndex;
 };
-#endif //_OSHASHTABLE_H_
+#endif 
