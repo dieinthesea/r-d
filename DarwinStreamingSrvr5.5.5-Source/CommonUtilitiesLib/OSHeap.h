4327f65585b0c8ccf4de9bff150c0bc2724b9a96
Universe51129 <91587729+Universe51129@users.noreply.github.com>
2023-05-01 16:11:42 +0800

    Update OSHeap.h

--- DarwinStreamingSrvr5.5.5-Source/CommonUtilitiesLib/OSHeap.h
+++ DarwinStreamingSrvr5.5.5-Source/CommonUtilitiesLib/OSHeap.h
@@ -1,35 +1,3 @@
-/*
- *
- * @APPLE_LICENSE_HEADER_START@
- * 
- * Copyright (c) 1999-2003 Apple Computer, Inc.  All Rights Reserved.
- * 
- * This file contains Original Code and/or Modifications of Original Code
- * as defined in and that are subject to the Apple Public Source License
- * Version 2.0 (the 'License'). You may not use this file except in
- * compliance with the License. Please obtain a copy of the License at
- * http://www.opensource.apple.com/apsl/ and read it before using this
- * file.
- * 
- * The Original Code and all software distributed under the License are
- * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
- * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
- * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
- * Please see the License for the specific language governing rights and
- * limitations under the License.
- * 
- * @APPLE_LICENSE_HEADER_END@
- *
- */
-/*
-    File:       OSHeap.h
-
-    Contains:   Implements a heap
-                    
-
-*/
-
 #ifndef _OSHEAP_H_
 #define _OSHEAP_H_
 
@@ -51,21 +19,15 @@ class OSHeap
         OSHeap(UInt32 inStartSize = kDefaultStartSize);
         ~OSHeap() { if (fHeap != NULL) delete fHeap; }
         
-        //ACCESSORS
         UInt32      CurrentHeapSize() { return fFreeIndex - 1; }
         OSHeapElem* PeekMin() { if (CurrentHeapSize() > 0) return fHeap[1]; return NULL; }
         
-        //MODIFIERS
-        
-        //These are the two primary operations supported by the heap
-        //abstract data type. both run in log(n) time.
         void            Insert(OSHeapElem*  inElem);
         OSHeapElem*     ExtractMin() { return Extract(1); }
         //removes specified element from the heap
         OSHeapElem*     Remove(OSHeapElem* elem);
         
 #if _OSHEAP_TESTING_
-        //returns true if it passed the test, false otherwise
         static Bool16       Test();
 #endif
     
@@ -74,7 +36,6 @@ class OSHeap
         OSHeapElem*     Extract(UInt32 index);
     
 #if _OSHEAP_TESTING_
-        //verifies that the heap is in fact a heap
         void            SanityCheck(UInt32 root);
 #endif
     
@@ -90,11 +51,6 @@ class OSHeapElem
             : fValue(0), fEnclosingObject(enclosingObject), fCurrentHeap(NULL) {}
         ~OSHeapElem() {}
         
-        //This data structure emphasizes performance over extensibility
-        //If it were properly object-oriented, the compare routine would
-        //be virtual. However, to avoid the use of v-functions in this data
-        //structure, I am assuming that the objects are compared using a 64 bit number.
-        //
         void    SetValue(SInt64 newValue) { fValue = newValue; }
         SInt64  GetValue()              { return fValue; }
         void*   GetEnclosingObject()    { return fEnclosingObject; }
@@ -109,4 +65,4 @@ class OSHeapElem
         
         friend class OSHeap;
 };
-#endif //_OSHEAP_H_
+#endif 
