ef16d26778f5ca742712e150b3463b16653b8a4d
Universe51129 <91587729+Universe51129@users.noreply.github.com>
2023-05-01 15:18:45 +0800

    Update OSCond.h

--- DarwinStreamingSrvr5.5.5-Source/CommonUtilitiesLib/OSCond.h
+++ DarwinStreamingSrvr5.5.5-Source/CommonUtilitiesLib/OSCond.h
@@ -1,37 +1,3 @@
-/*
- *
- * @APPLE_LICENSE_HEADER_START@
- * 
- * Copyright (c) 1999-2003 Apple Computer, Inc.  All Rights Reserved.
- * 
- * This file contains Original Code and/or Modifications of Original Code
- * as defined in and that are subject to the Apple Public Source License
- * Version 2.0 (the 'License'). You may not use this file except in
- * compliance with the License. Please obtain a copy of the License at
- * http://www.opensource.apple.com/apsl/ and read it before using this
- * file.
- * 
- * The Original Code and all software distributed under the License are
- * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
- * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
- * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
- * Please see the License for the specific language governing rights and
- * limitations under the License.
- * 
- * @APPLE_LICENSE_HEADER_END@
- *
- */
-/*
-    File:       OSCond.h
-
-    Contains:   A simple condition variable abstraction
-    
-    
-        
-
-*/
-
 #ifndef _OSCOND_H_
 #define _OSCOND_H_
 
@@ -106,12 +72,7 @@ inline void OSCond::Signal()
 inline void OSCond::Broadcast()
 {
 #ifdef __Win32__
-    //
-    // There doesn't seem like any more elegant way to
-    // implement Broadcast using events in Win32.
-    // This will work, it may generate spurious wakeups,
-    // but condition variables are allowed to generate
-    // spurious wakeups
+   
     UInt32 waitCount = fWaitCount;
     for (UInt32 x = 0; x < waitCount; x++)
     {
@@ -126,4 +87,4 @@ inline void OSCond::Broadcast()
 #endif
 }
 
-#endif //_OSCOND_H_
+#endif 
