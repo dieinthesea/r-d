83e10bc3dada404f459ee8819b8511878224ae0d
Universe51129 <91587729+Universe51129@users.noreply.github.com>
2023-05-01 22:01:14 +0800

    Update SocketUtils.h

--- DarwinStreamingSrvr5.5.5-Source/CommonUtilitiesLib/SocketUtils.h
+++ DarwinStreamingSrvr5.5.5-Source/CommonUtilitiesLib/SocketUtils.h
@@ -1,35 +1,3 @@
-/*
- *
- * @APPLE_LICENSE_HEADER_START@
- * 
- * Copyright (c) 1999-2003 Apple Computer, Inc.  All Rights Reserved.
- * 
- * This file contains Original Code and/or Modifications of Original Code
- * as defined in and that are subject to the Apple Public Source License
- * Version 2.0 (the 'License'). You may not use this file except in
- * compliance with the License. Please obtain a copy of the License at
- * http://www.opensource.apple.com/apsl/ and read it before using this
- * file.
- * 
- * The Original Code and all software distributed under the License are
- * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
- * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
- * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
- * Please see the License for the specific language governing rights and
- * limitations under the License.
- * 
- * @APPLE_LICENSE_HEADER_END@
- *
- */
-/*
-    File:       SocketUtils.h
-
-    Contains:   Some static routines for dealing with networking
-                    
-
-*/
-
 #ifndef __SOCKETUTILS_H__
 #define __SOCKETUTILS_H__
 
@@ -48,7 +16,7 @@
 
 #ifdef __solaris__
     #ifndef INADDR_NONE
-        #define INADDR_NONE     0xffffffff      /* -1 return from inet_addr */
+        #define INADDR_NONE     0xffffffff     
     #endif
 #endif
 
@@ -56,20 +24,15 @@ class SocketUtils
 {
     public:
 
-        // Call initialize before using any socket functions.
-        // (pass true for lookupDNSName if you want the hostname
-        // looked up via DNS during initialization -- %%sfu)
+        //initialize before using any socket functions.
         static void Initialize(Bool16 lookupDNSName = true);
         
         //static utility routines
         static Bool16   IsMulticastIPAddr(UInt32 inAddress);
         static Bool16   IsLocalIPAddr(UInt32 inAddress);
 
-        //This function converts an integer IP address to a dotted-decimal string.
-        //This function is NOT THREAD SAFE!!!
         static void ConvertAddrToString(const struct in_addr& theAddr, StrPtrLen* outAddr);
         
-        // This function converts a dotted-decimal string IP address to a UInt32
         static UInt32 ConvertStringToAddr(const char* inAddr);
         
         //You can get at all the IP addrs and DNS names on this machine this way
@@ -80,7 +43,6 @@ class SocketUtils
             
     private:
 
-        //Utility function used by Initialize
 #ifndef __Win32__
         static Bool16 IncrementIfReqIter(char** inIfReqIter, ifreq* ifr);
 #endif
@@ -118,5 +80,5 @@ inline StrPtrLen*   SocketUtils::GetDNSNameStr(UInt32 inDNSIndex)
     return &sIPAddrInfoArray[inDNSIndex].fDNSNameStr;
 }
 
-#endif // __SOCKETUTILS_H__
+#endif 
 
