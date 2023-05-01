bbd1330a4fec91c06b8d6834272841dae32408d0
Universe51129 <91587729+Universe51129@users.noreply.github.com>
2023-05-01 21:25:23 +0800

    Update OSThread.cpp

--- DarwinStreamingSrvr5.5.5-Source/CommonUtilitiesLib/OSThread.cpp
+++ DarwinStreamingSrvr5.5.5-Source/CommonUtilitiesLib/OSThread.cpp
@@ -1,36 +1,3 @@
-/*
- *
- * @APPLE_LICENSE_HEADER_START@
- * 
- * Copyright (c) 1999-2003 Apple Computer, Inc.  All Rights Reserved.
- * 
- * This file contains Original Code and/or Modifications of Original Code
- * as defined in and that are subject to the Apple Public Source License
- * Version 2.0 (the 'License'). You may not use this file except in
- * compliance with the License. Please obtain a copy of the License at
- * http://www.opensource.apple.com/apsl/ and read it before using this
- * file.
- * 
- * The Original Code and all software distributed under the License are
- * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
- * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
- * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
- * Please see the License for the specific language governing rights and
- * limitations under the License.
- * 
- * @APPLE_LICENSE_HEADER_END@
- *
- */
-/*
-    File:       OSThread.cpp
-
-    Contains:   Thread abstraction implementation
-
-    
-    
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "SafeStdLib.h"
@@ -60,9 +27,6 @@
 #endif
 
 
-//
-// OSThread.cp
-//
 void*   OSThread::sMainThreadData = NULL;
 
 #ifdef __Win32__
@@ -92,12 +56,8 @@ void OSThread::Initialize()
     pthread_key_create(&OSThread::gMainKey, NULL);
 #ifdef _POSIX_THREAD_PRIORITY_SCHEDULING
 
-    //
-    // Added for Solaris...
-    
     pthread_attr_init(&sThreadAttr);
-    /* Indicate we want system scheduling contention scope. This
-       thread is permanently "bound" to an LWP */
+ 
     pthread_attr_setscope(&sThreadAttr, PTHREAD_SCOPE_SYSTEM);
 #endif
 
@@ -121,7 +81,7 @@ OSThread::~OSThread()
 void OSThread::Start()
 {
 #ifdef __Win32__
-    unsigned int theId = 0; // We don't care about the identifier
+    unsigned int theId = 0; 
     fThreadID = (HANDLE)_beginthreadex( NULL,   // Inherit security
                                         0,      // Inherit stack size
                                         _Entry, // Entry function
@@ -132,7 +92,6 @@ void OSThread::Start()
 #elif __PTHREADS__
     pthread_attr_t* theAttrP;
 #ifdef _POSIX_THREAD_PRIORITY_SCHEDULING
-    //theAttrP = &sThreadAttr;
     theAttrP = 0;
 #else
     theAttrP = NULL;
@@ -153,8 +112,7 @@ void OSThread::StopAndWaitForThread()
 
 void OSThread::Join()
 {
-    // What we're trying to do is allow the thread we want to delete to complete
-    // running. So we wait for it to stop.
+    //waiting for the thread that we want to delete to complete running(waiting for it stop)
     Assert(!fJoined);
     fJoined = true;
 #ifdef __Win32__
@@ -170,8 +128,7 @@ void OSThread::Join()
 
 void OSThread::ThreadYield()
 {
-    // on platforms who's threading is not pre-emptive yield 
-    // to another thread
+
 #if THREADING_IS_COOPERATIVE
     #if __PTHREADS__
         #if USE_THR_YIELD
@@ -199,7 +156,6 @@ void OSThread::Sleep(UInt32 inMsec)
     SInt64 timeSlept = 0;
 
     do {
-        //qtss_printf("OSThread::Sleep = %qd request sleep=%qd\n",timeSlept, timeLeft);
         timeLeft = inMsec - timeSlept;
         if (timeLeft < 1)
             break;
@@ -207,18 +163,17 @@ void OSThread::Sleep(UInt32 inMsec)
         ::usleep(timeLeft * 1000);
 
         timeSlept = (OS::Milliseconds() - startTime);
-        if (timeSlept < 0) // system time set backwards
+        if (timeSlept < 0)
             break;
             
     } while (timeSlept < inMsec);
 
-	//qtss_printf("total sleep = %qd request sleep=%lu\n", timeSlept,inMsec);
 
 #elif defined(__osf__) || defined(__hpux__)
     if (inMsec < 1000)
-        ::usleep(inMsec * 1000); // useconds must be less than 1,000,000
+        ::usleep(inMsec * 1000); 
     else
-        ::sleep((inMsec + 500) / 1000); // round to the nearest whole second
+        ::sleep((inMsec + 500) / 1000); 
 #elif defined(__sgi__) 
 	struct timespec ts;
 	
@@ -264,12 +219,9 @@ Bool16  OSThread::SwitchPersonality()
         struct group* gr = ::getgrnam(sGroup);
         if (gr == NULL || ::setgid(gr->gr_gid) == -1)
         {
-            //qtss_printf("thread %lu setgid  to group=%s FAILED \n", (UInt32) this, sGroup);
             return false;
         }
-        
-        //qtss_printf("thread %lu setgid  to group=%s \n", (UInt32) this, sGroup);
-    }
+            }
     
         
     if (::strlen(sUser) > 0)
@@ -277,11 +229,9 @@ Bool16  OSThread::SwitchPersonality()
         struct passwd* pw = ::getpwnam(sUser);
         if (pw == NULL || ::setuid(pw->pw_uid) == -1)
         {
-            //qtss_printf("thread %lu setuid  to user=%s FAILED \n", (UInt32) this, sUser);
-            return false;
+		return false;
         }
 
-        //qtss_printf("thread %lu setuid  to user=%s \n", (UInt32) this, sUser);
    }
 #endif
 
@@ -304,13 +254,6 @@ OSThread*   OSThread::GetCurrent()
 int OSThread::GetErrno()
 {
     int winErr = ::GetLastError();
-
-    
-    // Convert to a POSIX errorcode. The *major* assumption is that
-    // the meaning of these codes is 1-1 and each Winsock, etc, etc
-    // function is equivalent in errors to the POSIX standard. This is 
-    // a big assumption, but the server only checks for a small subset of
-    // the real errors, on only a small number of functions, so this is probably ok.
     switch (winErr)
     {
 
