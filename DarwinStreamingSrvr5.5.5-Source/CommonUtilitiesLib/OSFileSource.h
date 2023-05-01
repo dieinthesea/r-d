2c3656597bf67b6277ce1af2e69043ef90c37cc5
Universe51129 <91587729+Universe51129@users.noreply.github.com>
2023-05-01 15:23:09 +0800

    Update OSFileSource.h

--- DarwinStreamingSrvr5.5.5-Source/CommonUtilitiesLib/OSFileSource.h
+++ DarwinStreamingSrvr5.5.5-Source/CommonUtilitiesLib/OSFileSource.h
@@ -1,36 +1,3 @@
-/*
- *
- * @APPLE_LICENSE_HEADER_START@
- * 
- * Copyright (c) 1999-2003 Apple Computer, Inc.  All Rights Reserved.
- * 
- * This file contains Original Code and/or Modifications of Original Code
- * as defined in and that are subject to the Apple Public Source License
- * Version 2.0 (the 'License'). You may not use this file except in
- * compliance with the License. Please obtain a copy of the License at
- * http://www.opensource.apple.com/apsl/ and read it before using this
- * file.
- * 
- * The Original Code and all software distributed under the License are
- * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
- * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
- * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
- * Please see the License for the specific language governing rights and
- * limitations under the License.
- * 
- * @APPLE_LICENSE_HEADER_END@
- *
- */
-/*
-    File:       osfilesource.h
-
-    Contains:   simple file abstraction. This file abstraction is ONLY to be
-                used for files intended for serving 
-                    
-    
-*/
-
 #ifndef __OSFILE_H_
 #define __OSFILE_H_
 
@@ -68,8 +35,8 @@ class FileBlockBuffer
 class FileBlockPool
 {
     enum {
-            kDataBufferUnitSizeExp      = 15,// base 2 exponent
-            kBufferUnitSize             = (1 << kDataBufferUnitSizeExp ) // 32Kbytes
+            kDataBufferUnitSizeExp      = 15,
+            kBufferUnitSize             = (1 << kDataBufferUnitSizeExp ) 
     };
 
     public:
@@ -171,8 +138,7 @@ class OSFileSource
         // Call this if you don't want Close or the destructor to close the fd
         void            DontCloseFD() { fShouldClose = false; }
         
-        //Advise: this advises the OS that we are going to be reading soon from the
-        //following position in the file
+     
         void            Advise(UInt64 advisePos, UInt32 adviseAmt);
         
         OS_Error    Read(void* inBuffer, UInt32 inLength, UInt32* outRcvLen = NULL)
@@ -231,4 +197,4 @@ class OSFileSource
 
 };
 
-#endif //__OSFILE_H_
+#endif 
