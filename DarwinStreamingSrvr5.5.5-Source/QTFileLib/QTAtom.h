3e2c8090e9e1bc7d5998e7e9e6796e0a08d3f301
Sylvia-qiu <91591335+Sylvia-qiu@users.noreply.github.com>
2023-05-03 00:20:05 +0800

    Update QTAtom.h
    删除头部注释

--- DarwinStreamingSrvr5.5.5-Source/QTFileLib/QTAtom.h
+++ DarwinStreamingSrvr5.5.5-Source/QTFileLib/QTAtom.h
@@ -1,55 +1,24 @@
-/*
- *
- * @APPLE_LICENSE_HEADER_START@
- * 
- * Copyright (c) 1999-2003 Apple Computer, Inc.  All Rights Reserved.
- * 
- * This file contains Original Code and/or Modifications of Original Code
- * as defined in and that are subject to the Apple Public Source License
- * Version 2.0 (the 'License'). You may not use this file except in
- * compliance with the License. Please obtain a copy of the License at
- * http://www.opensource.apple.com/apsl/ and read it before using this
- * file.
- * 
- * The Original Code and all software distributed under the License are
- * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
- * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
- * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
- * Please see the License for the specific language governing rights and
- * limitations under the License.
- * 
- * @APPLE_LICENSE_HEADER_END@
- *
- */
-//
 // QTAtom:
 //   The base-class for atoms in a QuickTime file.
 
 #ifndef QTAtom_H
 #define QTAtom_H
 
-
-//
 // Includes
 #include "OSHeaders.h"
 
 #include "QTFile.h"
 
 
-//
-// QTAtom class
 class QTAtom {
 
 public:
-    //
     // Constructors and destructor.
                         QTAtom(QTFile * File, QTFile::AtomTOCEntry * Atom,
                                Bool16 Debug = false, Bool16 DeepDebug = false);
     virtual             ~QTAtom(void);
 
 
-    //
     // Initialization functions.
     virtual Bool16      Initialize(void) { return true; }
 
@@ -65,7 +34,6 @@ public:
     #endif
     }
 
-    //
     // Read functions.
             Bool16      ReadBytes(UInt64 Offset, char * Buffer, UInt32 Length);
             Bool16      ReadInt8(UInt64 Offset, UInt8 * Datum);
@@ -83,7 +51,7 @@ public:
             
             char*       MemMap(UInt64 Offset, UInt32 Length);
             Bool16      UnMap(char *memPtr, UInt32 Length);
-    //
+    
     // Debugging functions.
     virtual void        DumpAtom(void) {}
 
