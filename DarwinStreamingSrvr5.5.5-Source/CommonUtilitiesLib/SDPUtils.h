87eb392aac3145c27ae4f07beb17fd49b75d5b85
Universe51129 <91587729+Universe51129@users.noreply.github.com>
2023-05-02 16:15:47 +0800

    Update SDPUtils.h

--- DarwinStreamingSrvr5.5.5-Source/CommonUtilitiesLib/SDPUtils.h
+++ DarwinStreamingSrvr5.5.5-Source/CommonUtilitiesLib/SDPUtils.h
@@ -1,35 +1,3 @@
-/*
- *
- * @APPLE_LICENSE_HEADER_START@
- * 
- * Copyright (c) 1999-2003 Apple Computer, Inc.  All Rights Reserved.
- * 
- * This file contains Original Code and/or Modifications of Original Code
- * as defined in and that are subject to the Apple Public Source License
- * Version 2.0 (the 'License'). You may not use this file except in
- * compliance with the License. Please obtain a copy of the License at
- * http://www.opensource.apple.com/apsl/ and read it before using this
- * file.
- * 
- * The Original Code and all software distributed under the License are
- * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
- * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
- * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
- * Please see the License for the specific language governing rights and
- * limitations under the License.
- * 
- * @APPLE_LICENSE_HEADER_END@
- *
- */
-/*
- File:       SDPUtils.h
-
- Contains:   Some static routines for dealing with SDPs
-
-
- */
-
 #ifndef __SDPUtilsH__
 #define __SDPUtilsH__
 
@@ -103,7 +71,7 @@ public:
     StrPtrLen   fSDPBuffer;
     UInt16      fReqLines;
 
-    char        fFieldStr[kLineTypeArraySize]; // 
+    char        fFieldStr[kLineTypeArraySize];
 
 };
 
@@ -124,8 +92,8 @@ public:
 	ResizeableStringFormatter fSDPMediaHeaders;
 	StrPtrLen fSessionHeaders;
 	StrPtrLen fMediaHeaders;
-	static char sSessionOrderedLines[];// = "vosiuepcbtrzka"; // chars are order dependent: declared by rfc 2327
-	static char sessionSingleLines[];//  = "vosiuepcbzk";    // return only 1 of each of these session field types
+	static char sSessionOrderedLines[];// = "vosiuepcbtrzka"; 
+	static char sessionSingleLines[];//  = "vosiuepcbzk";   
 	static StrPtrLen sEOL;
     static StrPtrLen sMaxBandwidthTag;
 };
