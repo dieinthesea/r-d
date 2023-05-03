22b97a705c94a522d394fefe32322cabb9c3d021
Universe51129 <91587729+Universe51129@users.noreply.github.com>
2023-05-03 16:43:37 +0800

    Update md5digest.h

--- DarwinStreamingSrvr5.5.5-Source/CommonUtilitiesLib/md5digest.h
+++ DarwinStreamingSrvr5.5.5-Source/CommonUtilitiesLib/md5digest.h
@@ -1,35 +1,3 @@
-/*
- *
- * @APPLE_LICENSE_HEADER_START@
- * 
- * Copyright (c) 1999-2003 Apple Computer, Inc.  All Rights Reserved.
- * 
- * This file contains Original Code and/or Modifications of Original Code
- * as defined in and that are subject to the Apple Public Source License
- * Version 2.0 (the 'License'). You may not use this file except in
- * compliance with the License. Please obtain a copy of the License at
- * http://www.opensource.apple.com/apsl/ and read it before using this
- * file.
- * 
- * The Original Code and all software distributed under the License are
- * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
- * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
- * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
- * Please see the License for the specific language governing rights and
- * limitations under the License.
- * 
- * @APPLE_LICENSE_HEADER_END@
- *
- */
-/*
-    File:       md5digest.h
-
-    Contains:   Provides a function to calculate the md5 digest 
-                given all the authentication parameters.
-                    
-*/
-
 #ifndef _MD5DIGEST_H_
 #define _MD5DIGEST_H_
 
@@ -74,7 +42,6 @@ void CalcRequestDigest( StrPtrLen* hA1,
 
 void to64(register char *s, register long v, register int n);
 
-// Doesn't allocate any memory. The size of the result buffer should be nbytes
 void MD5Encode( char *pw, char *salt, char *result, int nbytes);
 
 #endif
