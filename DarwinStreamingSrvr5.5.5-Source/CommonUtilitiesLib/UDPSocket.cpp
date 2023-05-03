621a6ca9898f958abecd784170f133afa342dc0a
Universe51129 <91587729+Universe51129@users.noreply.github.com>
2023-05-03 15:48:13 +0800

    Update UDPSocket.cpp

--- DarwinStreamingSrvr5.5.5-Source/CommonUtilitiesLib/UDPSocket.cpp
+++ DarwinStreamingSrvr5.5.5-Source/CommonUtilitiesLib/UDPSocket.cpp
@@ -1,36 +1,3 @@
-/*
- *
- * @APPLE_LICENSE_HEADER_START@
- * 
- * Copyright (c) 1999-2003 Apple Computer, Inc.  All Rights Reserved.
- * 
- * This file contains Original Code and/or Modifications of Original Code
- * as defined in and that are subject to the Apple Public Source License
- * Version 2.0 (the 'License'). You may not use this file except in
- * compliance with the License. Please obtain a copy of the License at
- * http://www.opensource.apple.com/apsl/ and read it before using this
- * file.
- * 
- * The Original Code and all software distributed under the License are
- * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
- * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
- * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
- * Please see the License for the specific language governing rights and
- * limitations under the License.
- * 
- * @APPLE_LICENSE_HEADER_END@
- *
- */
-/*
-    File:       UDPSocket.cpp
-
-    Contains:   Implementation of object defined in UDPSocket.h.
-
-    
-    
-*/
-
 #ifndef __Win32__
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -80,7 +47,6 @@ UDPSocket::SendTo(UInt32 inRemoteAddr, UInt16 inRemotePort, void* inBuffer, UInt
 #ifdef __sgi__
 	int theErr = ::sendto(fFileDesc, inBuffer, inLength, 0, (sockaddr*)&theRemoteAddr, sizeof(theRemoteAddr));
 #else
-    // Win32 says that inBuffer is a char*
 	int theErr = ::sendto(fFileDesc, (char*)inBuffer, inLength, 0, (sockaddr*)&theRemoteAddr, sizeof(theRemoteAddr));
 #endif
 
@@ -105,7 +71,6 @@ OS_Error UDPSocket::RecvFrom(UInt32* outRemoteAddr, UInt16* outRemotePort,
 #ifdef __sgi__
 	SInt32 theRecvLen = ::recvfrom(fFileDesc, ioBuffer, inBufLen, 0, (sockaddr*)&fMsgAddr, &addrLen);
 #else
-    // Win32 says that ioBuffer is a char*
     SInt32 theRecvLen = ::recvfrom(fFileDesc, (char*)ioBuffer, inBufLen, 0, (sockaddr*)&fMsgAddr, &addrLen);
 #endif
 
@@ -122,7 +87,7 @@ OS_Error UDPSocket::RecvFrom(UInt32* outRemoteAddr, UInt16* outRemotePort,
 OS_Error UDPSocket::JoinMulticast(UInt32 inRemoteAddr)
 {
     struct ip_mreq  theMulti;
-        UInt32 localAddr = fLocalAddr.sin_addr.s_addr; // Already in network byte order
+        UInt32 localAddr = fLocalAddr.sin_addr.s_addr; 
 
 #if __solaris__
     if( localAddr == htonl(INADDR_ANY) )
@@ -131,7 +96,6 @@ OS_Error UDPSocket::JoinMulticast(UInt32 inRemoteAddr)
     theMulti.imr_multiaddr.s_addr = htonl(inRemoteAddr);
     theMulti.imr_interface.s_addr = localAddr;
     int err = setsockopt(fFileDesc, IPPROTO_IP, IP_ADD_MEMBERSHIP, (char*)&theMulti, sizeof(theMulti));
-    //AssertV(err == 0, OSThread::GetErrno());
     if (err == -1)
          return (OS_Error)OSThread::GetErrno();
     else
@@ -140,9 +104,7 @@ OS_Error UDPSocket::JoinMulticast(UInt32 inRemoteAddr)
 
 OS_Error UDPSocket::SetTtl(UInt16 timeToLive)
 {
-    // set the ttl
-    u_char  nOptVal = (u_char)timeToLive;//dms - stevens pp. 496. bsd implementations barf
-                                            //unless this is a u_char
+    u_char  nOptVal = (u_char)timeToLive;
     int err = setsockopt(fFileDesc, IPPROTO_IP, IP_MULTICAST_TTL, (char*)&nOptVal, sizeof(nOptVal));
     if (err == -1)
         return (OS_Error)OSThread::GetErrno();
@@ -152,7 +114,7 @@ OS_Error UDPSocket::SetTtl(UInt16 timeToLive)
 
 OS_Error UDPSocket::SetMulticastInterface(UInt32 inLocalAddr)
 {
-    // set the outgoing interface for multicast datagrams on this socket
+    
     in_addr theLocalAddr;
     theLocalAddr.s_addr = inLocalAddr;
     int err = setsockopt(fFileDesc, IPPROTO_IP, IP_MULTICAST_IF, (char*)&theLocalAddr, sizeof(theLocalAddr));
