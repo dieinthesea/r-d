1571c304c12ddda27370a03f8917616d1dd5118d
Universe51129 <91587729+Universe51129@users.noreply.github.com>
2023-05-03 15:22:31 +0800

    Update TimeoutTask.cpp

--- DarwinStreamingSrvr5.5.5-Source/CommonUtilitiesLib/TimeoutTask.cpp
+++ DarwinStreamingSrvr5.5.5-Source/CommonUtilitiesLib/TimeoutTask.cpp
@@ -1,35 +1,3 @@
-/*
- *
- * @APPLE_LICENSE_HEADER_START@
- * 
- * Copyright (c) 1999-2003 Apple Computer, Inc.  All Rights Reserved.
- * 
- * This file contains Original Code and/or Modifications of Original Code
- * as defined in and that are subject to the Apple Public Source License
- * Version 2.0 (the 'License'). You may not use this file except in
- * compliance with the License. Please obtain a copy of the License at
- * http://www.opensource.apple.com/apsl/ and read it before using this
- * file.
- * 
- * The Original Code and all software distributed under the License are
- * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
- * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
- * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
- * Please see the License for the specific language governing rights and
- * limitations under the License.
- * 
- * @APPLE_LICENSE_HEADER_END@
- *
- */
-/*
-    File:       TimeoutTask.cpp
-
-    Contains:   Implementation of TimeoutTask
-                    
-    
-*/
-
 #include "TimeoutTask.h"
 #include "OSMemory.h"
 
@@ -53,7 +21,7 @@ TimeoutTask::TimeoutTask(Task* inTask, SInt64 inTimeoutInMilSecs)
     this->SetTimeout(inTimeoutInMilSecs);
     if (NULL == inTask)
 		fTask = (Task *) this;
-    Assert(sThread != NULL); // this can happen if RunServer intializes tasks in the wrong order
+    Assert(sThread != NULL); 
 
     OSMutexLocker locker(&sThread->fMutex); 
     sThread->fQueue.EnQueue(&fQueueElem);
@@ -76,10 +44,10 @@ void TimeoutTask::SetTimeout(SInt64 inTimeoutInMilSecs)
 
 SInt64 TimeoutTaskThread::Run()
 {
-    //ok, check for timeouts now. Go through the whole queue
+    //Go through the whole queue to check for timeout
     OSMutexLocker locker(&fMutex);
     SInt64 curTime = OS::Milliseconds();
-	SInt64 intervalMilli = kIntervalSeconds * 1000;//always default to 60 seconds but adjust to smallest interval > 0
+	SInt64 intervalMilli = kIntervalSeconds * 1000;
 	SInt64 taskInterval = intervalMilli;
 	
     for (OSQueueIter iter(&fQueue); !iter.IsDone(); iter.Next())
@@ -90,7 +58,7 @@ SInt64 TimeoutTaskThread::Run()
         if ((theTimeoutTask->fTimeoutAtThisTime > 0) && (curTime >= theTimeoutTask->fTimeoutAtThisTime))
         {
 #if TIMEOUT_DEBUGGING
-            qtss_printf("TimeoutTask %ld timed out. Curtime = %"_64BITARG_"d, timeout time = %"_64BITARG_"d\n",(SInt32)theTimeoutTask, curTime, theTimeoutTask->fTimeoutAtThisTime);
+            sss_printf("TimeoutTask %ld timed out. Curtime = %"_64BITARG_"d, timeout time = %"_64BITARG_"d\n",(SInt32)theTimeoutTask, curTime, theTimeoutTask->fTimeoutAtThisTime);
 #endif
 			theTimeoutTask->fTask->Signal(Task::kTimeoutEvent);
 		}
@@ -104,7 +72,7 @@ SInt64 TimeoutTaskThread::Run()
 #endif
 		}
 	}
-	(void)this->GetEvents();//we must clear the event mask!
+	(void)this->GetEvents();
 	
 	OSThread::ThreadYield();
 	
@@ -112,5 +80,5 @@ SInt64 TimeoutTaskThread::Run()
 	qtss_printf ("TimeoutTaskThread::Run interval seconds= %ld\n", (SInt32) intervalMilli/1000);
 #endif
     
-    return intervalMilli;//don't delete me!
+    return intervalMilli;
 }
