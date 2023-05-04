b90545d80abed445b4b9276dabbe8e7950b61411
Sylvia-qiu <91591335+Sylvia-qiu@users.noreply.github.com>
2023-05-04 18:03:11 +0800

    Update RTPSessionOutput.h
    更改注释

--- DarwinStreamingSrvr5.5.5-Source/APIModules/QTSSReflectorModule/RTPSessionOutput.h
+++ DarwinStreamingSrvr5.5.5-Source/APIModules/QTSSReflectorModule/RTPSessionOutput.h
@@ -1,27 +1,11 @@
 /*
- *
- * @APPLE_LICENSE_HEADER_START@
- * 
- * Copyright (c) 1999-2003 Apple Computer, Inc.  All Rights Reserved.
- * 
- * This file contains Original Code and/or Modifications of Original Code
- * as defined in and that are subject to the Apple Public Source License
- * Version 2.0 (the 'License'). You may not use this file except in
- * compliance with the License. Please obtain a copy of the License at
- * http://www.opensource.apple.com/apsl/ and read it before using this
- * file.
- * 
- * The Original Code and all software distributed under the License are
- * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
- * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
- * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
- * Please see the License for the specific language governing rights and
- * limitations under the License.
- * 
- * @APPLE_LICENSE_HEADER_END@
- *
- */
+RTPSessionOutput inherits ReflectorOutput 
+and includes some other classes and header files. 
+The main function of this class is to write RTP packets into the correct RTPStreamObject, 
+and track and filter RTP and RTCP packets when needed. 
+It includes functions such as WritePacket and PacketMatchesStream, 
+which are used to write and match data packets.
+*/
 /*
     File:       RTSPReflectorOutput.h
 
