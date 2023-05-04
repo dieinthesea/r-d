e3633b0187c46540354a45dc2e9d7d365129e3eb
Sylvia-qiu <91591335+Sylvia-qiu@users.noreply.github.com>
2023-05-04 18:18:34 +0800

    Update RelaySession.h
    更改注释

--- DarwinStreamingSrvr5.5.5-Source/APIModules/QTSSReflectorModule/RelaySession.h
+++ DarwinStreamingSrvr5.5.5-Source/APIModules/QTSSReflectorModule/RelaySession.h
@@ -1,27 +1,13 @@
 /*
- *
- * @APPLE_LICENSE_HEADER_START@
- * 
- * Copyright (c) 1999-2003 Apple Computer, Inc.  All Rights Reserved.
- * 
- * This file contains Original Code and/or Modifications of Original Code
- * as defined in and that are subject to the Apple Public Source License
- * Version 2.0 (the 'License'). You may not use this file except in
- * compliance with the License. Please obtain a copy of the License at
- * http://www.opensource.apple.com/apsl/ and read it before using this
- * file.
- * 
- * The Original Code and all software distributed under the License are
- * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
- * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
- * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
- * Please see the License for the specific language governing rights and
- * limitations under the License.
- * 
- * @APPLE_LICENSE_HEADER_END@
- *
- */
+Inherited from the ReflectorSession class. 
+It has some static methods, such as Register() and Initialize(),
+which are used to call in the registration and initialization role of the relay module.
+It also has some properties and functions, 
+such as SetupReplaySession() for setting up relay sessions, 
+and GetReplaySessionObject () for obtaining relay session objects.
+The instantiation of this class requires passing in a pointer to a StrPtrLen type object 
+and an optional pointer to a SourceInfo type object
+*/
 /*
     File:       RelaySession.h
 
