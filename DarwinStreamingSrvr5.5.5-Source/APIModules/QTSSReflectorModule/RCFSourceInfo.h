eda4a321a6fe610c63499230f3279ce5dbe6a74e
Sylvia-qiu <91591335+Sylvia-qiu@users.noreply.github.com>
2023-05-04 17:58:18 +0800

    Update RCFSourceInfo.h
    更改注释

--- DarwinStreamingSrvr5.5.5-Source/APIModules/QTSSReflectorModule/RCFSourceInfo.h
+++ DarwinStreamingSrvr5.5.5-Source/APIModules/QTSSReflectorModule/RCFSourceInfo.h
@@ -1,27 +1,16 @@
 /*
- *
- * @APPLE_LICENSE_HEADER_START@
- * 
- * Copyright (c) 1999-2003 Apple Computer, Inc.  All Rights Reserved.
- * 
- * This file contains Original Code and/or Modifications of Original Code
- * as defined in and that are subject to the Apple Public Source License
- * Version 2.0 (the 'License'). You may not use this file except in
- * compliance with the License. Please obtain a copy of the License at
- * http://www.opensource.apple.com/apsl/ and read it before using this
- * file.
- * 
- * The Original Code and all software distributed under the License are
- * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
- * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
- * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
- * Please see the License for the specific language governing rights and
- * limitations under the License.
- * 
- * @APPLE_LICENSE_HEADER_END@
- *
- */
+A class called RCFSourceInfo is defined, 
+inheriting from the SourceInfo class. 
+The role of this class is to construct the StreamInfo structure by parsing the SDP data. 
+This class contains functions for parsing the SDP file and constructing the OutputInfo structure.
+Among other things, 
+the SetName function is used to set the name of RCFSourceInfo and the Name function returns 
+the name of RCFSourceInfo. 
+The class also has protected functions 
+such as ParseDestination and ParseAnnouncedDestination for parsing targets 
+and announced targets. Finally, the class also defines a pointer of type char, fName, 
+as the name of the RCFSourceInfo.
+*/
 /*
     File:       RCFSourceInfo.h
 
