06a5c66f5a2f29869d743ad365abc2cb12a4dad5
Sylvia-qiu <91591335+Sylvia-qiu@users.noreply.github.com>
2023-05-05 15:59:43 +0800

    Update ReflectorSession.h
    更改注释

--- DarwinStreamingSrvr5.5.5-Source/APIModules/QTSSReflectorModule/ReflectorSession.h
+++ DarwinStreamingSrvr5.5.5-Source/APIModules/QTSSReflectorModule/ReflectorSession.h
@@ -1,27 +1,7 @@
 /*
- *
- * @APPLE_LICENSE_HEADER_START@
- * 
- * Copyright (c) 1999-2003 Apple Computer, Inc.  All Rights Reserved.
- * 
- * This file contains Original Code and/or Modifications of Original Code
- * as defined in and that are subject to the Apple Public Source License
- * Version 2.0 (the 'License'). You may not use this file except in
- * compliance with the License. Please obtain a copy of the License at
- * http://www.opensource.apple.com/apsl/ and read it before using this
- * file.
- * 
- * The Original Code and all software distributed under the License are
- * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
- * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
- * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
- * Please see the License for the specific language governing rights and
- * limitations under the License.
- * 
- * @APPLE_LICENSE_HEADER_END@
- *
- */
+The header file of C++code contains two classes: FileDelete and ReflectorSession. 
+Used for automatically deleting files and for forwarding RTP packets, respectively.
+*/
 /*
     File:       ReflectorSession.h
 
@@ -30,12 +10,7 @@
                 maximize the randomness of the sending pattern and smooth
                 the stream.
                     
-    
-
-*/
-
-        
-
+ */
 #include "QTSS.h"
 #include "OSRef.h"
 #include "StrPtrLen.h"
@@ -46,10 +21,10 @@
 #include "SourceInfo.h"
 #include "OSArrayObjectDeleter.h"
 
-
 #ifndef _FILE_DELETER_
 #define _FILE_DELETER_
 
+//Used to automatically delete files
 class FileDeleter
 {   public:
         FileDeleter(StrPtrLen* inSDPPath);
@@ -61,35 +36,35 @@ class FileDeleter
 
 #endif
 
+//Used to achieve the forwarding of RTP packets.
 
+//Public interfaces include initialization functions, 
+//adding output functions, removing output functions, 
+//and functions for obtaining some information. 
+//In addition, this class also includes some enumeration types and constant definitions.
 #ifndef __REFLECTOR_SESSION__
 #define __REFLECTOR_SESSION__
 class ReflectorSession
 {
     public:
     
-        // Public interface to generic RTP packet forwarding engine
-        
-        //
-        // Initialize
-        //
+        // Public interface to generic RTP packet forwarding engine        
         // Call initialize before calling any other function in this class
         static void Initialize();
             
         // Create one of these ReflectorSessions per source broadcast. For mapping purposes,
         // the object can be constructred using an optional source ID.
-        //
+        
         // Caller may also provide a SourceInfo object, though it is not needed and
         // will also need to be provided to SetupReflectorSession when that is called.
         ReflectorSession(StrPtrLen* inSourceID, SourceInfo* inInfo = NULL);
         virtual ~ReflectorSession();
         
-        //
         // MODIFIERS
         
         // Call this to initialize and setup the source sockets. Once this function
         // completes sucessfully, Outputs may be added through the function calls below.
-        //
+        
         // The SourceInfo object passed in here will be owned by the ReflectorSession. Do not
         // delete it.
         
@@ -111,13 +86,11 @@ class ReflectorSession
         void    RemoveSessionFromOutput(QTSS_ClientSessionObject inSession);
         void    ManuallyMarkSetup() { fIsSetup = true; }
         
-        // For the Relay's status, a ReflectorSession can format an informative bit of
-        // HTML to describe the source. This must be called after the ReflectorSession
-        // is all setup.
+        // For the Relay's status, a ReflectorSession can format an informative bit of HTML to describe the source.
+        // This must be called after the ReflectorSession is all setup.
         
         void    FormatHTML(StrPtrLen* inURL);
 
-        //
         // ACCESSORS
         
         OSRef*          GetRef()            { return &fRef; }
@@ -137,17 +110,16 @@ class ReflectorSession
         void            SetSocketStream(QTSS_StreamRef inStream)    { fSocketStream = inStream; }
         QTSS_StreamRef  GetSocketStream()                           { return fSocketStream; }
         
-        // A ReflectorSession keeps track of the aggregate bit rate each
-        // stream is reflecting (RTP only). Initially, this will return 0
+        // A ReflectorSession keeps track of the aggregate bit rate each stream is reflecting (RTP only).
+        // Initially, this will return 0
         // until enough time passes to compute an accurate average.
         UInt32          GetBitRate();
         
-        // Returns true if this SourceInfo structure is equivalent to this
-        // ReflectorSession.
+        // Returns true if this SourceInfo structure is equivalent to this ReflectorSession.
         Bool16 Equal(SourceInfo* inInfo);
         
-        // Each stream has a cookie associated with it. When the stream writes a packet
-        // to an output, this cookie is used to identify which stream is writing the packet.
+        // Each stream has a cookie associated with it. When the stream writes a packet to an output, 
+        // this cookie is used to identify which stream is writing the packet.
         // The below function is useful so outputs can get the cookie value for a stream ID,
         // and therefore mux the cookie to the right output stream.
         void*   GetStreamCookie(UInt32 inStreamID);
@@ -167,7 +139,7 @@ class ReflectorSession
      
     private:
     
-        // Is this session setup?
+        // session setup or not
         Bool16      fIsSetup;
 
         // For storage in the session map       
@@ -183,8 +155,7 @@ class ReflectorSession
         StrPtrLen   fSourceInfoHTML;
         ResizeableStringFormatter fFormatter;
         
-        // The reflector session needs to hang onto the source info object
-        // for it's entire lifetime. Right now, this is used for reflector-as-client.
+        // The reflector session needs to hang onto the source info object for it's entire lifetime. Right now, this is used for reflector-as-client.
         SourceInfo* fSourceInfo;
         StrPtrLen fLocalSDP;
         
