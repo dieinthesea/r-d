16e5ae6fc199f5017d10305ea0a933091b0a9cca
errorBlackie <125293685+errorBlackie@users.noreply.github.com>
2023-05-11 06:29:28 +0800

    Update RTSPRequest.cpp

--- DarwinStreamingSrvr5.5.5-Source/Server.tproj/RTSPRequest.cpp
+++ DarwinStreamingSrvr5.5.5-Source/Server.tproj/RTSPRequest.cpp
@@ -1,28 +1,4 @@
 /*
- *
- * @APPLE_LICENSE_HEADER_START@
- * 
- * Copyright (c) 1999-2003 Apple Computer, Inc.  All Rights Reserved.
- * 
- * This file contains Original Code and/or Modifications of Original Code
- * as defined in and that are subject to the Apple Public Source License
- * Version 2.0 (the 'License'). You may not use this file except in
- * compliance with the License. Please obtain a copy of the License at
- * http://www.opensource.apple.com/apsl/ and read it before using this
- * file.
- * 
- * The Original Code and all software distributed under the License are
- * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
- * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
- * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
- * Please see the License for the specific language governing rights and
- * limitations under the License.
- * 
- * @APPLE_LICENSE_HEADER_END@
- *
- */
-/*
     File:       RTSPRequest.cpp
 
     Contains:   Implementation of RTSPRequest class.
@@ -98,13 +74,13 @@ static StrPtrLen    sQuoteCommaSpace("\", ", 3);
 static StrPtrLen    sStaleTrue("stale=\"true\", ", 14); 
 
 //Parses the request
-QTSS_Error RTSPRequest::Parse()
+SSS_Error RTSPRequest::Parse()
 {
     StringParser parser(this->GetValue(qtssRTSPReqFullRequest));
-    Assert(this->GetValue(qtssRTSPReqFullRequest)->Ptr != NULL);
+    Assert(this->GetValue(SSSRTSPReqFullRequest)->Ptr != NULL);
 
     //parse status line.
-    QTSS_Error error = ParseFirstLine(parser);
+    SSS_Error error = ParseFirstLine(parser);
 
     //handle any errors that come up    
     if (error != QTSS_NoErr)
