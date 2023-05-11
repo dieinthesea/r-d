61e2f1ebb67388067f24cfaf048c0925760d269e
Universe51129 <91587729+Universe51129@users.noreply.github.com>
2023-05-11 22:16:05 +0800

    Update SDPSourceInfo.h

--- DarwinStreamingSrvr5.5.5-Source/APICommonCode/SDPSourceInfo.h
+++ DarwinStreamingSrvr5.5.5-Source/APICommonCode/SDPSourceInfo.h
@@ -1,26 +1,3 @@
-/*
-This code defines a class called SDPSourceInfo.
-It inherits from the SourceInfo class.
-It contains a constructor that constructs a StreamInfo structure using SDP data.
-It provides methods for parsing SDP files and getting local SDP files.
-In addition, it has a GetSDPData method for getting SDP data.
-In the private part of the class, it defines an enumeration constant kDefaultTTL.
-and declares a StrPtrLen object called fSDPData.
-*/
-/*
-    File:       SDPSourceInfo.h
-
-    Contains:   This object takes input SDP data, and uses it to support the SourceInfo
-                API.
-*/
-
-#ifndef __SDP_SOURCE_INFO_H__
-#define __SDP_SOURCE_INFO_H__
-
-#include "StrPtrLen.h"
-#include "SourceInfo.h"
-#include "StringParser.h"
-
 class SDPSourceInfo : public SourceInfo
 {
     public:
@@ -33,18 +10,11 @@ class SDPSourceInfo : public SourceInfo
         // Parses out the SDP file provided, sets up the StreamInfo structures
         void    Parse(char* sdpData, UInt32 sdpLen);
 
-        // This function uses the Parsed SDP file, and strips out all the network information,
-        // producing an SDP file that appears to be local.
+        // This function uses the Parsed SDP file, and strips out all the network information,producing an SDP file that appears to be local.
         virtual char*   GetLocalSDP(UInt32* newSDPLen);
 
         // Returns the SDP data
         StrPtrLen*  GetSDPData()    { return &fSDPData; }
-        
-        // Utility routines
-        
-        // Assuming the parser is currently pointing at the beginning of an dotted-
-        // decimal IP address, this consumes it (stopping at inStopChar), and returns
-        // the IP address (host ordered) as a UInt32
         static UInt32 GetIPAddr(StringParser* inParser, char inStopChar);
       
     private:
