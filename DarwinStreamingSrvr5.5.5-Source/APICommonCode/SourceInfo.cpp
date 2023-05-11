00cf3f1494fa4c3705301cbcc4186e363b854ed8
Universe51129 <91587729+Universe51129@users.noreply.github.com>
2023-05-11 22:20:18 +0800

    Update SourceInfo.cpp

--- DarwinStreamingSrvr5.5.5-Source/APICommonCode/SourceInfo.cpp
+++ DarwinStreamingSrvr5.5.5-Source/APICommonCode/SourceInfo.cpp
@@ -1,35 +1,3 @@
-/*
- *
- * @APPLE_LICENSE_HEADER_START@
- * 
- * Copyright (c) 1999-2003 Apple Computer, Inc.  All Rights Reserved.
- * 
- * This file contains Original Code and/or Modifications of Original Code
- * as defined in and that are subject to the Apple Public Source License
- * Version 2.0 (the 'License'). You may not use this file except in
- * compliance with the License. Please obtain a copy of the License at
- * http://www.opensource.apple.com/apsl/ and read it before using this
- * file.
- * 
- * The Original Code and all software distributed under the License are
- * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
- * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
- * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
- * Please see the License for the specific language governing rights and
- * limitations under the License.
- * 
- * @APPLE_LICENSE_HEADER_END@
- *
- */
-/*
-    File:       SourceInfo.cpp
-
-    Contains:   Implements object defined in .h file.
-                    
-
-*/
-
 #include "SourceInfo.h"
 #include "SocketUtils.h"
 #include "SDPSourceInfo.h"
@@ -189,21 +157,13 @@ Bool16  SourceInfo::SetActiveNTPTimes(UInt32 startTimeNTP,UInt32 endTimeNTP)
         accepted = true;
         
     }  while(0);
-    
-    //char buffer[kTimeStrSize];
-    //qtss_printf("SourceInfo::SetActiveNTPTimes fStartTimeUnixSecs=%lu fEndTimeUnixSecs=%lu\n",fStartTimeUnixSecs,fEndTimeUnixSecs);
-    //qtss_printf("SourceInfo::SetActiveNTPTimes start time = %s",qtss_ctime(&fStartTimeUnixSecs, buffer, sizeof(buffer)) );
-    //qtss_printf("SourceInfo::SetActiveNTPTimes end time = %s",qtss_ctime(&fEndTimeUnixSecs, buffer, sizeof(buffer)) );
+
     fHasValidTime = accepted;
     return accepted;
 }
 
 Bool16  SourceInfo::IsActiveTime(time_t unixTimeSecs)
 { 
-    // order of tests are important here
-    // we do it this way because of the special case time value of 0 for end time
-    // start - 0 = unbounded 
-    // 0 - 0 = permanent
     if (false == fHasValidTime)
         return false;
         
