1c8ea1cf222280626b92cd60ed7a9d4b45fa3e24
Universe51129 <91587729+Universe51129@users.noreply.github.com>
2023-05-11 22:09:56 +0800

    Update QTSSMemoryDeleter.h

--- DarwinStreamingSrvr5.5.5-Source/APICommonCode/QTSSMemoryDeleter.h
+++ DarwinStreamingSrvr5.5.5-Source/APICommonCode/QTSSMemoryDeleter.h
@@ -1,23 +1,3 @@
-/*
-    A template class called QTSSMemoryDeleter is defined to delete heap objects at the end of their life cycle. The class has the following methods:
-
-    Constructor: initialises the fT member to the passed pointer parameter.
-    Destructor: calls the QTSS_Delete method to delete the heap object pointed to by fT at the end of the object's life cycle.
-    ClearObject method: sets the fT member to NULL to prevent the object from being deleted at the end of its life cycle.
-    SetObject method: sets the fT member to the passed pointer parameter, but requires fT to be NULL to ensure that objects are not accidentally deleted.
-    GetObject method: returns a pointer to the object pointed to by the fT member.
-    Type conversion operator: allows the QTSSMemoryDeleter object to be converted to a pointer of type T.
-    In addition, the code defines a QTSSCharArrayDeleter type for deleting heap objects of type char.
-  
-*/
-/*
-    File:       QTSSMemoryDeleter.h
-
-    Contains:   Auto object for deleting memory allocated by QTSS API callbacks,
-                such as QTSS_GetValueAsString
-
-*/
-
 #ifndef __QTSS_MEMORY_DELETER_H__
 #define __QTSS_MEMORY_DELETER_H__
 
