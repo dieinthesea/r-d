46edf629b045b7846ff2e36480f0deb5288d229b
Universe51129 <91587729+Universe51129@users.noreply.github.com>
2023-05-12 15:31:20 +0800

    Update timestamp.s

--- DarwinStreamingSrvr5.5.5-Source/AtomicLib/timestamp.s
+++ DarwinStreamingSrvr5.5.5-Source/AtomicLib/timestamp.s
@@ -1,47 +1,5 @@
-/*
- * Copyright (c) 2003 Apple Computer, Inc. All rights reserved.
- *
- * @APPLE_LICENSE_HEADER_START@
- * 
- * Copyright (c) 1999-2003 Apple Computer, Inc.  All Rights Reserved.
- * 
- * This file contains Original Code and/or Modifications of Original Code
- * as defined in and that are subject to the Apple Public Source License
- * Version 2.0 (the 'License'). You may not use this file except in
- * compliance with the License. Please obtain a copy of the License at
- * http://www.opensource.apple.com/apsl/ and read it before using this
- * file.
- * 
- * The Original Code and all software distributed under the License are
- * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
- * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
- * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
- * Please see the License for the specific language governing rights and
- * limitations under the License.
- * 
- * @APPLE_LICENSE_HEADER_END@
- */
-/*
- *
- *	File:	timestamp.s
- *
- *	History:
- *		12-Feb-1999 Umesh Vaishampayan (umeshv@apple.com)
- *			Integrated scaledtimestamp() written by
- *			Joe Sokol (sokol1@apple.com)
- *
- *		23-Oct-1998	Umesh Vaishampayan (umeshv@apple.com)
- *			Created.
- */
-
 #include <architecture/ppc/asm_help.h>
 
-/*
- *	long long timestamp(void)
- *
- *		Read the PPC timebase.
- */
 LEAF(_timestamp)
 Lagain:
 	mftbu   r3
@@ -50,17 +8,6 @@ Lagain:
 	cmpw    r6, r3
 	bne-    Lagain
 	blr
-
-
-/*
- *	long long scaledtimestamp(double scale)
- *
- *		Read the PPC timebase. Convert the time base value based on
- *		scale.
- *
- *		Caveat: scale can not be 0, NaN, Inf. It's upto the caller
- *				to validate scale before calling this.
- */
 
 LEAF(_scaledtimestamp)
 Lagain1:
